6a8f8f00ab00d6202ab8d1a2095af1a287a5d1a2
Diego Felipe <diegofelipe.14@gmail.com>
2018-11-27 19:12:11 -0400

    Adding the Caesar Cipher Algorithm

---
+++ caesarCipher.js
@@ -0,0 +1,41 @@
+var validCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('')
+
+const removeInvalidCharacters = (str, validCharacters) => {
+    resultArray = []
+
+    str.filter(character => {
+        if(validCharacters.indexOf(character) > -1) resultArray.push(character)
+    })
+
+    return resultArray
+}
+
+const getCharIndexOnArray = (char, arrayToFind)  => arrayToFind.indexOf(char)
+const getIndexValidCharacters = str => getCharIndexOnArray(str, validCharacters)
+
+const getNextPosition = (currentPosition, incrementor) => {
+
+    let possibleNextPos = currentPosition + incrementor
+    let nextPos = possibleNextPos >= validCharacters.length ? 1 : possibleNextPos;
+
+    return nextPos
+}
+
+const getCipheredChar = position => validCharacters[position]
+
+const caesarCipher = (str, num) => {
+
+    str = str.split('')
+    /* 3 steps:
+    1 - Remove invalid characters
+    2 - Get current position in the valid characters array
+    3 - Get Next Valid Position
+    4 - Get the character at the next valid position (according to num)
+    5 - Join the result and return the result
+    */
+    str = removeInvalidCharacters(str, validCharacters).map(getIndexValidCharacters).map(index => getNextPosition(index, num)).map(getCipheredChar).join('')
+    return str
+
+}
+
+console.log(caesarCipher('zoo keeper', 2)) //
