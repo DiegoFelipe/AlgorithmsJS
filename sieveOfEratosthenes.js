13c832aa9c752b76475b75f77c12ec5e2e0c3512
Diego Felipe <diegofelipe.14@gmail.com>
2018-12-22 10:25:59 -0400

    fixing sieveOfEratosthenes

--- sieveOfEratosthenes.js
+++ sieveOfEratosthenes.js
@@ -1,23 +1,38 @@
 const sieveOfEratosthenes = num => {
-    let primes = createArray(num, true)
+    let primes = createArrayWithValue(num, true)
     primes = markArrFalse(primes, [0,1])
 
-    console.log(primes)
+    for (var i = 2; i <= Math.sqrt(num); i++) {
+        for (var j = 2; j * i <= num; j++) {
+            primes[i * j] = false
+        }
+    }
+
+    let result = removeFalseFromArray(primes)
+    return result
 }
 
-const createArray = (num, val) => {
+const removeFalseFromArray = arr => {
+    let result = []
+    arr.forEach((el, index) => {
+        if(el) result.push(index)
+    })
+    return result
+}
+
+const createArrayWithValue = (amount, val) => {
     var arr = []
-    for(let i =0; i < num; i++) {
+    for(let i =0; i < amount; i++) {
         arr.push(val)
     }
     return arr
 }
 
 const markArrFalse = (arr, indexesToDelete) => {
-    arr.forEach((ind, val) => {
-        console.log(ind, val)
-        val = indexesToDelete.includes(ind) ? false : val})
-
+    arr.forEach((val, ind) => {
+       val = indexesToDelete.includes(ind) ? false : arr[ind]
+        arr[ind] = val
+    })
     return arr
 }
 
